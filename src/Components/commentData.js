705f236dc74111979990e5c6027577e02826207b
lackingworth <worstface@yandex.ru>
2023-06-22 17:42:17 +0300

    Commited source code

---
+++ src/Components/commentData.js
@@ -0,0 +1,17 @@
+export const comments = [
+    {
+      profileImg: 'https://images.unsplash.com/photo-1609992556706-14a7056ea745?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+      username: 'UserName1',
+      comment: 'Comment1'
+    },
+    {
+      profileImg: 'https://images.unsplash.com/photo-1615751072497-5f5169febe17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80',
+      username: 'UserName2',
+      comment: 'Comment2'
+    },
+    {
+      profileImg: 'https://images.unsplash.com/photo-1563482776068-4dac10f9373d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      username: 'UserName3',
+      comment: 'Comment3'
+    }
+  ]
